41eaccd3a3b3618d0599c16323f029f0157caa4a
Denton DeLoss <denton.deloss@gmail.com>
2024-03-03 23:28:57 -0800

    Update keybindings

--- dotfiles/nvim/lua/dd/core/keymaps.lua
+++ dotfiles/nvim/lua/dd/core/keymaps.lua
@@ -30,3 +30,6 @@ keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" 
 keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
 keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
+
+-- Neogit
+keymap.set("n", "<leader>gg", ":Neogit<CR>", { desc = "Open Neogit" })
