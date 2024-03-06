bbed354b0287245e0d4f0ee1c8bf164b780c2e35
Denton DeLoss <denton.deloss@gmail.com>
2024-03-05 19:12:33 -0800

    Remove increment/decrement keymaps

--- dotfiles/nvim/lua/dd/core/keymaps.lua
+++ dotfiles/nvim/lua/dd/core/keymaps.lua
@@ -15,10 +15,6 @@ keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
 -- delete single character without copying into register
 -- keymap.set("n", "x", '"_x')
 
--- increment/decrement numbers
-keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" }) -- increment
-keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" }) -- decrement
-
 -- window management
 keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
 keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
